f572e25b1661605cad3e737ad96c7a42eeebd3b4
Chris <amayachristopher55@gmail.com>
2025-12-20 11:15:19 -0500

    Initial commit

---
+++ src/main/java/net/cosmic/azure_origins/AzureOriginsClient.java
@@ -0,0 +1,10 @@
+package net.cosmic.azure_origins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AzureOriginsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cosmic/azure_origins/AzureOriginsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cosmic.azure_origins;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AzureOriginsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
